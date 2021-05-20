0265a6841e8666716ff4ed2b85f711cd46d984f6
github username <github email address>
2021-05-20 13:06:51 -0400

    append items  to main div

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function getItems(){
     .then(json => renderItems(json.data))
 };
 
-// function renderItems(items){ #items is 
-//     items.forEach(item => {
-//         const item = 
-          
-//     })
-//     debugger
-// };
+function renderItems(items){ 
+    items.forEach(item => {
+     const itemContainer = `
+        <div data-id=${item.id}> 
+          <img src=${item.attributes.image_url}>
+          <a href="${item.attributes.url}"><h3>${item.attributes.title}</h3></a>
+          <p><strong>Category:</strong> ${item.attributes.category.name}<p>
+          <p font-size:10px;>${item.attributes.description}</p>
+        </div>`;
+        const visionBoardContainer = document.getElementById("vision-board-container")
+
+        visionBoardContainer.innerHTML += itemContainer
+        
+    })
+
+
+
+
+
+};
 
