4fe7e60bafe4beac76983084e7ff33e2b5cbca27
github username <github email address>
2021-05-20 17:49:27 -0400

    work on post fetch request

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const endPoint = "http://localhost:3000/api/v1/items"
 const itemForm = document.getElementById("create-vision-board-item-form")
 
 document.addEventListener('DOMContentLoaded', function() {
-    getItems()
-    itemForm.addEventListener("submit", (event) => createFormHandler(event))
+    getItems();
+    itemForm.addEventListener("submit", (event) => createFormHandler(event));
 }); 
 
 
@@ -31,6 +31,32 @@ function renderItems(items){
 
 function createFormHandler(event){
   event.preventDefault()
-  console.log(event)
+  const titleInput = document.getElementById("item-title").value
+  const descriptionInput = document.getElementById("item-description").value
+  const imageInput = document.getElementById("item-link").value
+  const itemLink = document.getElementById("item-url").value
+  const categorySelection = document.getElementById("categories").value
+  const categoryId = parseInt(categorySelection)
+   
+  postFetchRequest(titleInput, descriptionInput, imageInput, itemLink, categoryId)
 };
+
+function postFetchRequest(title, description, image_url, url, category_id){
+    const formData = {
+        
+    }
+   
+   
+   
+    const configObj = {
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json",
+           "Accept": "application/json"
+       }
+       body: JSON.stringify(formData)
+   }
+
+
+}
 
