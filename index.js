d2c479afcf423ceb78107b99c85766270b09a1ce
github username <github email address>
2021-05-20 12:10:23 -0400

    fetch get request to index action works

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/api/v1/items"
+
+document.addEventListener('DOMContentLoaded', function() {
+    getItems()
+}); 
+
+function getItems(){
+    fetch(endPoint)
+    .then(r => r.json())
+    .then(json => renderItems(json.data))
+};
+
+// function renderItems(items){ #items is 
+//     items.forEach(item => {
+//         const item = 
+          
+//     })
+//     debugger
+// };
+
