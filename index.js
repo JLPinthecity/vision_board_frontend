e19b94a8a4088d9db1663c56707be19994d5882d
github username <github email address>
2021-05-20 16:32:00 -0400

    add event listener to form submit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const endPoint = "http://localhost:3000/api/v1/items"
+const itemForm = document.getElementById("create-vision-board-item-form")
 
 document.addEventListener('DOMContentLoaded', function() {
     getItems()
+    itemForm.addEventListener("submit", (event) => createFormHandler(event))
 }); 
+
 
 function getItems(){
     fetch(endPoint)
@@ -13,9 +16,9 @@ function getItems(){
 function renderItems(items){ 
     items.forEach(item => {
      const itemContainer = `
-        <div id="item" data-id=${item.id}> 
+        <div data-id=${item.id}> 
           <img src=${item.attributes.image_url}>
-          <a href="${item.attributes.url}"><h3>${item.attributes.title}</h3></a>
+          <a href="${item.attributes.url}" target="_blank"><h3>${item.attributes.title}</h3></a>
           <p><strong>Category:</strong> ${item.attributes.category.name}<p>
           <p font-size:10px;>${item.attributes.description}</p>
         </div>`;
@@ -24,10 +27,9 @@ function renderItems(items){
         visionBoardContainer.innerHTML += itemContainer
         
     })
+};
 
-
-
-
+function createFormHandler(){
 
 };
 
