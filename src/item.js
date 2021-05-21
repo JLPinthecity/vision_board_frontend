75a797167b810e3411e7ad47441b431404716191
github username <github email address>
2021-05-21 11:24:26 -0400

    build item constructoor

--- src/item.js
+++ src/item.js
@@ -1,3 +1,17 @@
 class Item {
 
+    static all = []
+
+    constructor({title, description, image_url, url, category_id}){
+    //setting up properties of item
+    this.title = title
+    this.description = description
+    this.image_url = image_url
+    this.url = url
+    this.category_id = category_id
+
+
+    //remembering all the items
+    Item.all.push(this)
+
 };
