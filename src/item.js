be7f09a5ea8add07a9c52fee03d7f245ff76d622
github username <github email address>
2021-05-21 11:36:24 -0400

    fix missing end input

--- src/item.js
+++ src/item.js
@@ -2,16 +2,18 @@ class Item {
 
     static all = []
 
-    constructor({title, description, image_url, url, category_id}){
+    constructor(data){
     //setting up properties of item
-    this.title = title
-    this.description = description
-    this.image_url = image_url
-    this.url = url
-    this.category_id = category_id
+        this.title = title
+        this.description = description
+        this.image_url = image_url
+        this.url = url
+        this.category_id = category_id
 
 
     //remembering all the items
     Item.all.push(this)
 
 };
+
+}
