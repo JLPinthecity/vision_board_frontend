22e7d451e5cf2288689504ffb5a7bad1508e2a1f
github username <github email address>
2021-05-22 10:05:40 -0400

    make categoryapi class and move index.js out of src folder

--- src/category.js
+++ src/category.js
@@ -2,4 +2,13 @@ class Category {
     
     static all = []
 
+    constructor 
+
 }
+
+
+//category class responsible for:
+//remembering all categories
+//setting up properties of each category
+//html we want on dom for each category
+//attach event listeners to category buttons
